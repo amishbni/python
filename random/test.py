e9630887ae0d6bc809b2fab57bb4be3529c7f65a
Amir A. Shabani <amirshabani011@gmail.com>
2019-11-12 16:45:05 +0330

    numpy.random.choice is no longer needed

--- random/test.py
+++ random/test.py
@@ -1,11 +1,18 @@
-from numpy.random import choice
+from random_selection import select
 from collections import Counter
 
-def select(array, total_count, probability):
+def test(array, total_count, probability):
     probability_dict = {}
-    rand_items = choice(array, total_count, p=probability)
+    rand_items = select(array, total_count, probability)
     items_counter = Counter(rand_items)
 
     for item, count in items_counter.most_common():
         probability_dict[item] = f"{100 * count / total_count:.1f}%"
     return probability_dict
+
+
+print(test(
+    ['a', 'b', 'c', 'd'],
+    1000,
+    [0.7, 0.1, 0.1, 0.1]
+))
