d9f37c7a08e9db941be6f4c8302c33bdbed13522
Amir A. Shabani <amirshabani011@gmail.com>
2019-11-12 16:58:23 +0330

    Choose which method applies

--- random/test.py
+++ random/test.py
@@ -1,18 +1,28 @@
 from random_selection import select
 from collections import Counter
+from numpy.random import choice
 
-def test(array, total_count, probability):
-    probability_dict = {}
-    rand_items = select(array, total_count, probability)
-    items_counter = Counter(rand_items)
+def test(array, total_count, probability, method):
+    methods = {
+        "numpy.random.choice": choice(array, total_count, probability),
+        "random_selection.select": select(array, total_count, probability)
+    }
 
-    for item, count in items_counter.most_common():
-        probability_dict[item] = f"{100 * count / total_count:.1f}%"
-    return probability_dict
+    if(method in methods):
+        probability_dict = {}
+        rand_items = methods[method]
+        items_counter = Counter(rand_items)
+
+        for item, count in items_counter.most_common():
+            probability_dict[item] = f"{100 * count / total_count:.1f}%"
+        return probability_dict
+    else:
+        raise ValueError(f"Method {method} has not been defined.")
 
 
 print(test(
     ['a', 'b', 'c', 'd'],
     1000000,
-    [0.7, 0.1, 0.1, 0.1]
+    [0.7, 0.1, 0.1, 0.1],
+    "random_selection.select"
 ))
