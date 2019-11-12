f173676db36b278095831e982a02b6e95ff6e6f7
Amir A. Shabani <amirshabani011@gmail.com>
2019-11-12 17:46:33 +0330

    Test numpy.random.choice vs random_selection.select

--- random/test.py
+++ random/test.py
@@ -1,10 +1,11 @@
 from random_selection import select
 from collections import Counter
 from numpy.random import choice
+from time import time
 
 def test(array, total_count, probability, method):
     methods = {
-        "numpy.random.choice": choice(array, total_count, probability),
+        "numpy.random.choice": choice(array, total_count, p=probability),
         "random_selection.select": select(array, total_count, probability)
     }
 
@@ -20,9 +21,32 @@ def test(array, total_count, probability, method):
         raise ValueError(f"Method {method} has not been defined.")
 
 
-print(test(
-    ['a', 'b', 'c', 'd'],
-    1000000,
-    [0.7, 0.1, 0.1, 0.1],
-    "random_selection.select"
-))
+def main():
+    total_count = 1000000
+    array = ['a', 'b', 'c', 'd']
+    probability = [0.7, 0.1, 0.1, 0.1]
+
+    print(f"array: {array}")
+    print(f"probability: {probability}")
+
+    print()
+
+    print('random_selection.select: ')
+    start_time = time()
+    result = test(array, total_count, probability, 'random_selection.select')
+    end_time = time()
+    print(result)
+    print(f"{(end_time - start_time):.4f} s")
+
+    print()
+
+    print('numpy.random.choice: ')
+    start_time = time()
+    result = test(array, total_count, probability, 'numpy.random.choice')
+    end_time = time()
+    print(result)
+    print(f"{(end_time - start_time):.4f} s")
+
+
+if __name__ == "__main__":
+    main()
