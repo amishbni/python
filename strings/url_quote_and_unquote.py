fd526bc4e80aa49147a05eb2bf81d85f77ed8658
Amir A. Shabani <amirshabani011@gmail.com>
2019-11-22 03:17:02 +0330

    Example for the use of quoting and unquoting URLs

---
+++ strings/url_quote_and_unquote.py
@@ -0,0 +1,6 @@
+# example for the use of quoting and unquoting urls
+
+from urllib.parse import quote, unquote
+
+print(f"In a URL, the letter & is written as {quote('&')}")
+print(f"If you see %2B in a URL, it represents the letter {unquote('%2B')}")
