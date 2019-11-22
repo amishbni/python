e41507faac11e49cc978f6b336742c1f4618c68f
Amir A. Shabani <amirshabani011@gmail.com>
2019-11-22 21:52:01 +0330

    Uncommented

--- data_types.py
+++ data_types.py
@@ -1,31 +1,31 @@
-# # Fundamental data types in Python
-# data_types = [int, float, bool, str, list, tuple, set, dict]
+# Fundamental data types in Python
+data_types = [int, float, bool, str, list, tuple, set, dict]
 
-# for data_type in data_types:
-#     print(data_type)
+for data_type in data_types:
+    print(data_type)
 
-# # None, absence of a value
-# print(type(None))
+# None, absence of a value
+print(type(None))
 
-# # int data type
-# print(f"2 + 4 is {2 + 4}")
-# print(f"2 - 4 is {2 - 4}")
-# print(f"2 * 4 is {2 * 4}")
-# print(f"2 / 4 is {2 / 4}")
-# print(f"2 // 4 is {2 // 4}")
-# print(f"2 % 4 is {2 % 4}")
-# print(f"2 ** 4 is {2 ** 4}")
+# int data type
+print(f"2 + 4 is {2 + 4}")
+print(f"2 - 4 is {2 - 4}")
+print(f"2 * 4 is {2 * 4}")
+print(f"2 / 4 is {2 / 4}")
+print(f"2 // 4 is {2 // 4}")
+print(f"2 % 4 is {2 % 4}")
+print(f"2 ** 4 is {2 ** 4}")
 
-# # float arithmetic
-# one_tenth = .1
-# print(one_tenth)                        # 0.1
-# print(one_tenth * 3)                    # 0.30000000000000004
+# float arithmetic
+one_tenth = .1
+print(one_tenth)                        # 0.1
+print(one_tenth * 3)                    # 0.30000000000000004
 
-# three_tenth = one_tenth * 3
-# print(three_tenth)                      # 0.30000000000000004
-# print(three_tenth == .3)                # False
-# print(round(one_tenth, 1) * 3 == .3)    # False
-# print(round(three_tenth, 1) == .3)      # True
+three_tenth = one_tenth * 3
+print(three_tenth)                      # 0.30000000000000004
+print(three_tenth == .3)                # False
+print(round(one_tenth, 1) * 3 == .3)    # False
+print(round(three_tenth, 1) == .3)      # True
 
 
 # as_integer_ratio() method
