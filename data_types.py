54f050f8bce8edd08022d15e1214bccee736bda5
Amir A. Shabani <amirshabani011@gmail.com>
2019-11-22 20:49:57 +0330

    Uncomment

--- data_types.py
+++ data_types.py
@@ -1,11 +1,11 @@
-# # Fundamental data types in Python
-# data_types = [int, float, bool, str, list, tuple, set, dict]
+# Fundamental data types in Python
+data_types = [int, float, bool, str, list, tuple, set, dict]
 
-# for data_type in data_types:
-#     print(data_type)
+for data_type in data_types:
+    print(data_type)
 
-# # None, absence of a value
-# print(type(None))
+# None, absence of a value
+print(type(None))
 
 # int data type
 print(f"2 + 4 is {2 + 4}")
