1a9e52419c554e2058027d11b04cb7b56278e1c4
Amir A. Shabani <amirshabani011@gmail.com>
2019-11-22 21:31:04 +0330

    Floating Point Arithmetic

--- data_types.py
+++ data_types.py
@@ -15,3 +15,14 @@ print(f"2 / 4 is {2 / 4}")
 print(f"2 // 4 is {2 // 4}")
 print(f"2 % 4 is {2 % 4}")
 print(f"2 ** 4 is {2 ** 4}")
+
+# float arithmetic
+one_tenth = .1
+print(one_tenth)                        # 0.1
+print(one_tenth * 3)                    # 0.30000000000000004
+
+three_tenth = one_tenth * 3
+print(three_tenth)                      # 0.30000000000000004
+print(three_tenth == .3)                # False
+print(round(one_tenth, 1) * 3 == .3)    # False
+print(round(three_tenth, 1) == .3)      # True
