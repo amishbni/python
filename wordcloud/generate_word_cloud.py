edc9af06a444ff76e76a0e6d95a324cdaebbf655
Amir A. Shabani <amirshabani011@gmail.com>
2020-01-09 17:02:12 +0330

    Generate wordcloud from a test file

---
+++ wordcloud/generate_word_cloud.py
@@ -0,0 +1,13 @@
+from wordcloud import WordCloud, STOPWORDS
+
+with open("test.txt", 'r') as input_file:
+    text = input_file.read()
+
+stopwords = set(STOPWORDS)
+
+wc = WordCloud(background_color="white", max_words=2000, stopwords=stopwords, contour_width=3, contour_color="steelblue")
+
+wc.generate(text)
+
+wc.to_file("wordcloud.png")
+
